a3a96c531a64e4f0cd39863479267f0c67fc8f09
Vishal Kumar <vishalk@contatan.local>
2024-05-07 19:53:15 +0530

    initial code pushed

---
+++ client/src/Redux/Actions/UserAction.js
@@ -0,0 +1,55 @@
+import axios from 'axios'
+import { server } from '../Store'
+
+//LOGIN
+export const loginUser = (employeeID, password, navigate) => async dispatch => {
+  try {
+    dispatch({
+      type: 'loginRequest'
+    })
+
+    const config = { headers: { 'Content-Type': 'application/json' } }
+
+    const { data } = await axios.post(
+      `${server}/login`,
+      { empId: employeeID, password },
+      config
+    )
+
+    dispatch({
+      type: 'loginSuccess',
+      payload: data
+    })
+    if (data.employee.role === 'admin') {
+      navigate('/admin/home')
+    } else if (data.employee.role === 'employee') {
+      navigate('/home')
+    } else {
+      console.error('Unknown user role:', data.employee.role)
+    }
+    return data
+  } catch (error) {
+    dispatch({
+      type: 'loginFail',
+      payload: error.response.data.title
+    })
+  }
+}
+
+//LOGOUT
+export const logout = () => async dispatch => {
+  try {
+    dispatch({
+      type: 'logoutRequest'
+    })
+
+    const { data } = await axios.get(`${server}/logout`, {
+      withCredentials: true
+    })
+
+    dispatch({ type: 'logoutSuccess', payload: data })
+    return data
+  } catch (error) {
+    dispatch({ type: 'logoutFail', payload: error.reponse.data.message })
+  }
+}
---
+++ server/server/Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+using server.Model;
+using server.Services;
+using System.Web.Http.Cors;
+
+namespace server.Controllers
+{
+    [ApiController]
+    [Route("/api/v1")]
+    public class UserController : Controller
+    {
+        private readonly UserService _userService;
+
+
+        public UserController(UserService userService)
+        {
+            _userService = userService;
+        }
+
+
+
+        //LOGIN USER 
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            return await _userService.login(user);
+        }
+
+        [HttpGet("logout")]
+        public async Task<IActionResult> Logout()
+        {
+            return Ok(new { success = true, message = "Logout successful" });
+        }
+
+    }
+}
